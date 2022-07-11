cb828de9e39ea66b9909cfaf78a45c26d06b37fa
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-07-11 14:59:13 +0200

    Add footer

---
+++ pages/layouts/_footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="devCredit">Made by <a href="http://github.com/AbdelrahmanDwedar/" target="_blank" rel="noopener noreferrer">Abdelrahman Dwedar</a></div>
+            </footer>
+        </>
+    );
+}
