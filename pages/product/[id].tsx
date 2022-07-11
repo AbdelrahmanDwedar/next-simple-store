d43abd5aad54d26949c4cf08a6fb805f06d2298b
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-07-11 15:07:48 +0200

    Add products dir
    Added the components (not yet made)

---
+++ pages/product/[id].tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+
+interface Product_templateProps {
+    
+}
+ 
+const Product_template: FunctionComponent<Product_templateProps> = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default Product_template;
---
+++ pages/product/index.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+
+interface ProductsProps {
+    
+}
+ 
+const Products: FunctionComponent<ProductsProps> = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default Products;
