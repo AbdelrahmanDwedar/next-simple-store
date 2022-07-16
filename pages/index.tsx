93289c28e413840b0f9d69aec8b6062468dc0700
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-07-17 00:00:05 +0200

    Feat the Cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,29 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.scss'
 import Header from './layouts/_header';
+import { Button, Col, Row } from "react-bootstrap";
+import storeItems from '../public/data/items.json';
+import { StoreItem } from './Components/_storeItem';
 
 const Home: NextPage = () => {
   return (
-  <>
-		<Header title="Home Page" />
-		<main className={ styles.main }>
-      <h1 className={ styles.title }></h1>
+    <>
+      <Header title="Home Page" />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Shopping List</h1>
 
-      <section className={ styles.mainContent}>
-        <div className={ styles.card }>
-          <img src="" alt="" className={ styles.cardImage } />
-          <div>
-            <h4 className={ styles.cardTitle }></h4>
-            <p className={ styles.cardDiscription }></p>
-            <button className={ styles.cardButton }>Add to Cart</button>
-          </div>
-        </div>
-      </section>
-    </main>
-	</>
+        <section className={styles.mainContent}>
+          <h1>Store</h1>
+          <Row md={2} xs={1} lg={3} className="g-3">
+            {storeItems.map(item => (
+              <Col key={item.id}>
+                <StoreItem {...item} />
+              </Col>
+            ))}
+          </Row>
+        </section>
+      </main>
+    </>
   );
 }
 
