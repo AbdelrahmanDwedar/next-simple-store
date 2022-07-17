ce8b95c9332c062d3f27236933496d7f3332bf76
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-07-17 20:22:03 +0200

    Fix about us page & styles

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,29 +1,32 @@
 import { FunctionComponent } from "react";
 import Header from "./layouts/_header";
+import styles from '../styles/about.module.scss';
+import { ShoppingCartProvider } from "./context/_shoppingCartContext";
+import { Card } from "react-bootstrap";
 
 interface AboutProps {
-    
+
 }
- 
+
 const About: FunctionComponent<AboutProps> = () => {
-    return ( 
-        <>
+    return (
+        <ShoppingCartProvider>
             <Header title="About Us" />
 
-            <main className="main">
-                <div className="about-project-section">
-                    <h3 className="about-heading">About This Project</h3>
-                    <p className="about-text">
-                        This project was made with &#10084; by <a href="http://github.com/AbdelrahmaDwedar" target="_blank" rel="noopener noreferrer">AbdelrahmaDwedar</a>. <br />
+            <main className={styles.main}>
+                <Card className={styles.aboutProjectSection}>
+                    <h3 className={styles.aboutHeading}>About This Project</h3>
+                    <p className={styles.aboutText}>
+                        This project was made with &#10084; by <a href="http://github.com/AbdelrahmanDwedar" target="_blank" rel="noopener noreferrer">AbdelrahmaDwedar</a>. <br />
                         This project is just a simple layout for a <strong>simple shope</strong>, it's not secure to use it as the backend or the server side of your project at all. <br />
 
                         If you used this layout be aware that only the layout is available for use, the server side is just a testing for <a href="http://https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</a>. <br />
                         If there was any issue that you ran through with the server side the owner of this repository is not responcable. &#9888;
                     </p>
-                </div>
+                </Card>
             </main>
-        </>
+        </ShoppingCartProvider>
      );
 }
- 
+
 export default About;
